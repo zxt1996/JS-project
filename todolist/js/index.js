c8a165cb7e3c8d43f5a297d96b35aacc680affff
zxt1996 <15625144271@163.com>
2019-11-11 14:29:49 +0800

    20191111

---
+++ todolist/js/index.js
@@ -0,0 +1,54 @@
+var addinput = document.querySelector('#add-task-input');
+var add = document.querySelector('#js-add-task');
+var content = document.querySelector('ul');
+let name;
+let template = "";
+
+add.addEventListener('click',function(){
+	let createli = document.createElement('li');
+	name = addinput.value;
+	template = `
+                <p>${name} </p>
+                <span class="iconfont icon-close close"></span>
+		`
+	if(name != ""){
+		createli.classList.add('task');
+		createli.innerHTML = template;
+		content.appendChild(createli);
+	}else{
+		addinput.placeholder = "请输入内容"
+	}
+	
+	forcheck();
+	let close = document.querySelectorAll('.close');
+	forclose(close);
+})
+
+function forcheck(){
+	let task = document.querySelectorAll('.task');
+	task.forEach(function(ele,index){
+		let forchange = true
+		ele.addEventListener('click',function(){
+			if(forchange){
+				ele.classList.add('checked');
+				forchange = !forchange;
+			}else{
+				ele.classList.remove('checked');
+				forchange = !forchange;
+			}
+			
+		})
+	})
+}
+
+forcheck();
+let close = document.querySelectorAll('.close');	
+function forclose(close){
+	close.forEach(function(ele,index){
+		ele.addEventListener('click',function(){
+			ele.parentNode.remove();
+		})
+	})
+}
+
+forclose(close);
